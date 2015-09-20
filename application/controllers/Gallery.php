6c5f6e02ae2cf3b342b48828143c99d48bb8d55f
Derek <Derek@192.168.200.128>
2015-09-19 20:49:17 -0700

    Completed tutorial #basic02: Convert Trivial Site to Basic CodeIgniter; also removed some old files no longer needed in public

--- application/controllers/Gallery.php
+++ application/controllers/Gallery.php
@@ -1,13 +1,15 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Gallery extends CI_Controller {
+class Gallery extends Application {
 
-	/**
-	 * Index Page for this controller.
-	 */
-	public function index()
-	{
-		$this->load->view('gallery');
-	}
+    /**
+     * Index Page for this controller.
+     */
+    public function index()
+    {
+        // $this->load->view('gallery');
+        $this->data['pagebody'] = 'gallery';
+        $this->render();
+    }
 }
